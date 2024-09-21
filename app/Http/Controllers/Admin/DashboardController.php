cf57b34973f15b56efe698d0125ea50f8df8a932
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-21 15:22:26 +0200

    feat: add view

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        dump('index della dashboard');
+        return view('admin.index');
     }
 }
