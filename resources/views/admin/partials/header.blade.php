d42d7d7508972597009daddd99f859c07fe156f6
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-23 21:38:24 +0200

    feat: add header elements

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,1 +1,13 @@
-<header>Header</header>
+<header>
+    <div class="container-fluid bg-secondary-subtle">
+        <nav class="row justify-content-between py-3">
+            <div class="col-2 pub">
+                <a href="#">Boolpress</a>
+            </div>
+            <div class="col-1 d-flex justify-content-end">
+                <a href="#">User</a>
+            </div>
+        </nav>
+
+    </div>
+</header>
