61c3de282df5238e452aa999c72179c76944936f
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-23 22:47:24 +0200

    add: route to nav bar

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -2,10 +2,27 @@
     <div class="container-fluid bg-secondary-subtle">
         <nav class="row justify-content-between py-3">
             <div class="col-2 pub">
-                <a href="#">Boolpress</a>
+                <a href="{{ route('home') }}">Boolpress</a>
             </div>
-            <div class="col-1 d-flex justify-content-end">
-                <a href="#">User</a>
+            <div class="col d-flex justify-content-end">
+                <ul class=" col-2 d-flex justify-content-between">
+                    @guest
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Registrati</a></li>
+                    @else
+                        <li>
+                            <a href="#">
+                                {{ Auth::user()->name }}
+                            </a>
+                        <li>
+                            {{-- <a href="{{ route('logout') }}">
+                                Logout
+                            </a> --}}
+                        </li>
+                        </li>
+                    @endguest
+                </ul>
+
             </div>
         </nav>
 
