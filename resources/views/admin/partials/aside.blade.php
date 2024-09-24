b1967f5fb6a44cc41a9ad73aa5119dd3bb46027e
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-24 19:49:06 +0200

    fix aside route

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,7 +1,11 @@
 <aside>
-    <ul class="d-flex flex-column ul-aside">
-        <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-house p-1"></a></i> Home</li>
-        <li><a href="{{ route('admin.posts.index') }}"><i class="fa-solid fa-list p-1"></a></i> Posts</li>
-        <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-plus p-1"></a></i> Nuovo post</li>
+
+    <ul class="">
+        <li class="ul-aside d-flex flex-column">
+            <a class="py-2" href="{{ route('admin.home') }}"><i class="fa-solid fa-house p-1"></i> Home</a>
+            <a class="py-2" href="{{ route('admin.posts.index') }}"><i class="fa-solid fa-list p-1"></i> Posts</a>
+            <a class="py-2" href="{{ route('admin.home') }}"><i class="fa-solid fa-plus p-1"></i> Nuovo post </a>
+        </li>
     </ul>
+
 </aside>
