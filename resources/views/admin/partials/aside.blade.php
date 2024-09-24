9063e50b39d2a5337b114545b9cd9fb5cf7148ec
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-24 16:53:42 +0200

    feat: add icon in aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,7 +1,7 @@
 <aside>
     <ul class="d-flex flex-column ul-aside">
-        <li>Home</li>
-        <li>Posts</li>
-        <li>Nuovo post</li>
+        <li><i class="fa-solid fa-house p-1"></i> Home</li>
+        <li><i class="fa-solid fa-list p-1"></i> Posts</li>
+        <li><i class="fa-solid fa-plus p-1"></i> Nuovo post</li>
     </ul>
 </aside>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Boolpress|Admin</title>
-
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.css'
+        integrity='sha512-8BU3emz11z9iF75b10oPjjpamM4Mz23yQFQymbtwyPN3mNWHxpgeqyrYnkIUP6A8KyAj5k2p3MiYLtYqew7gIw=='
+        crossorigin='anonymous' />
     @vite(['resources/js/app.js'])
 </head>
 
 <body>
 
     @include('admin.partials.header')
-    <div class="d-flex">
+    <div class="d-flex main">
         @include('admin.partials.aside')
         @yield('content')
     </div>
